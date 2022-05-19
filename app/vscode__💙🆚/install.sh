85e2af24e1fd6fc3f9a18b8cd0de77a154a9ef3d
b <b@elastic.ventures>
2022-05-19 10:28:45 +1000

    vscode extensions

---
+++ app/vscode__💙🆚/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# 🥾: install extensions
+# to update extensions:
+# code --list-extensions > extensions.txt
+
+
+## VS CODE
+# code --install-extension
+# code --new-window
+# code --reuse-window
+
+# exceedsystem.vscode-macros
+
+cat extensions.txt | xargs -L 1 -- code --install-extension $1
+
